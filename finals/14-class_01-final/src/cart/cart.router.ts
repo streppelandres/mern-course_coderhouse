32e88fa059274b7ca71e6fba270a3c518c38c968
streppelandres <andres.streppel@gmai.com>
2022-06-27 21:57:31 -0300

    Cart, delete y get by id

--- finals/14-class_01-final/src/cart/cart.router.ts
+++ finals/14-class_01-final/src/cart/cart.router.ts
@@ -7,7 +7,7 @@ const cartsRouter: Router = Router();
 // TODO: Mover esto, no deberia estar en router
 const container: Container = new Container('14-class_01-final_carts.json');
 
-// Capaz estaría bueno que reciba nada más los id's de los productos a guardar
+// TODO: Capaz estaría bueno que reciba nada más los id's de los productos a guardar
 // y en el get, antes de devolverlo, cargar todos los productos
 cartsRouter.post('/', async (req: Request, res: Response) => {
     try {
@@ -19,6 +19,30 @@ cartsRouter.post('/', async (req: Request, res: Response) => {
     } catch (error) {
         console.error(error);
         res.status(500).send({ message: 'No se pudo guardar el carrito' });
+    }
+});
+
+cartsRouter.delete('/:id', async (req: Request, res: Response) => {
+    const id: string = req.params.id;
+    try {
+        await container.deleteById(Number(id));
+        res.status(200).send({
+            message: `Carrito con el id ${id} eliminado`
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({ message: `No se pudo eliminar el carrito con el id ${id}` });
+    }
+});
+
+cartsRouter.get('/:id', async (req: Request, res: Response) => {
+    const id: string = req.params.id;
+    try {
+        const product: ContainerModel = await container.getById(Number(id));
+        res.status(200).send(product);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({ message: `No se pudo cargar el carrito con el id ${id}` });
     }
 });
 
